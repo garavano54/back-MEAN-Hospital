3cc2860d1ea50fce4676a487d93a29cabc25f12c
Alejandro Garavano Viotti <garavano54@gmail.com>
2020-10-04 16:44:08 -0300

    Fin ABM Medico y Hospital

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,12 +94,26 @@ const googleSignIn = async(req, res = response) => {
             msg: "Token no es correcto"
         });
     }
+}
 
 
+const renewToken = async(req, res = response) => {
+
+    const uid = req.uid;
+
+    // Generar el TOKEN - JWT
+    const token = await generarJWT(uid);
+
+    res.json({
+        ok: true,
+        uid,
+        token
+    });
 }
 
 
 module.exports = {
     login,
-    googleSignIn
+    googleSignIn,
+    renewToken
 }
--- routes/hospitales.js
+++ routes/hospitales.js
@@ -18,8 +18,12 @@ router.post('/', [
     validarCampos
 ], crearHospitales);
 
-router.put('/:id', [], actualizarHospitales);
+router.put('/:id', [
+    validarJWT,
+    check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
+    validarCampos
+], actualizarHospitales);
 
-router.delete('/:id', borrarHospitales);
+router.delete('/:id', validarJWT, borrarHospitales);
 
 module.exports = router;
